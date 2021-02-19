faad728b96c44484fff0ecdf819d13bb3126215a
ramonedtest <ramoned.mailer@gmail.com>
2021-02-19 10:07:52 +0100

    Segunda versión subida al server

--- js/firebase-messaging-sw.js
+++ js/firebase-messaging-sw.js
@@ -1,9 +1,17 @@
 importScripts("https://www.gstatic.com/firebasejs/8.2.6/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.2.6/firebase-messaging.js");
 
-firebase.initializeApp({
-    "messagingSenderId" : "313092521954"
-});
+var firebaseConfig = {
+    apiKey: "AIzaSyA1A5AmACIfWpjTUQ93HPv7wzuMK2evAvM",
+    authDomain: "pushnotifications-e9a0e.firebaseapp.com",
+    projectId: "pushnotifications-e9a0e",
+    storageBucket: "pushnotifications-e9a0e.appspot.com",
+    messagingSenderId: "313092521954",
+    appId: "1:313092521954:web:3e2724e8dba49e814803bb",
+    measurementId: "G-8ZB35LY1WL"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
